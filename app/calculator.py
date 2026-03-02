d067aea63b8e2049d6f6adcda39063c0dde1b2a7
Kiryl Tymanovich <tymanovich17@gmail.com>
2026-03-02 21:40:23 +0300

    fix1

--- app/calculator.py
+++ app/calculator.py
@@ -1,20 +1,7 @@
-def add(x, y): return x + y
-def subtract(x, y): return x - y
-def multiply(x, y): return x * y
-def divide(x, y): return x / y if y != 0 else "Ошибка: деление на ноль"
+def add (a: int, b: int):
+    return a + b
 
-print("--- Простой Калькулятор ---")
-try:
-    num1 = float(input("Введите первое число: "))
-    op = input("Выберите операцию (+, -, *, /): ")
-    num2 = float(input("Введите второе число: "))
-
-    operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
-
-    if op in operations:
-        result = operations[op](num1, num2)
-        print(f"Результат: {result}")
-    else:
-        print("Неверная операция!")
-except ValueError:
-    print("Ошибка: вводите только числа")
+def dev(a: int, b: int) -> float:
+    if b == 0:
+        raise ValueError("Division by zero")
+    return a / b
